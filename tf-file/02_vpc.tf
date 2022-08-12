24e597084df82ed27b523a70866163e8e3a09771
wlsdud <58159312+qus0905@users.noreply.github.com>
2022-08-12 20:13:26 +0900

    Delete tf-file directory

--- tf-file/02_vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "project_vpc" {
-  cidr_block = "12.0.0.0/16"
-  enable_dns_hostnames = true
-  tags={
-    Name="project-vpc"
-  }
-}
--- tf-file/11_1_db_subnet_gp.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_db_subnet_group" "tf_rdsg" {
-  name = "tf-rdsg"
-  subnet_ids = [aws_subnet.dba.id, aws_subnet.dbc.id]
-}
